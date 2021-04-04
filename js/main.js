bfa067dd1744e33982f3c44f5a891b916b5f84f4
dzhelez2903 <dzhelez@yahoo.com>
2021-04-04 23:52:01 +0300

    added js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+if (navigator.appVersion.toUpperCase().indexOf("MSIE") != -1 ||
+  navigator.appVersion.toUpperCase().indexOf("TRIDENT") != -1) {
+  alert(
+    "This page does not work with Internet Explorer, please follow the link and download the latest decent browser :) https://browsehappy.com/?locale=en"
+  );
+}
